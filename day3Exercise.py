1431bb607b493e119ec0f4ceb669048ae8cff263
mazhuang <mazhuang@xylink.com>
2019-08-05 19:35:12 +0800

    Create day3Exercise.py

---
+++ day3Exercise.py
@@ -0,0 +1,151 @@
+"""
+分段函数求值
+
+        3x - 5  (x > 1)
+f(x) =  x + 2   (-1 <= x <= 1)
+        5x + 3  (x < -1)
+
+
+"""
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+elif x>= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+print('f(%.2f) = %.2f' % (x, y))
+
+
+"""
+英制单位英寸和公制单位厘米互换
+
+
+"""
+value = float(input('请输入长度: '))
+unit = input('请输入单位： ')
+if unit == 'in' or unit == '英寸':
+    print('%f英寸 = %f厘米' % (value, value * 2.54))
+elif unit == 'cm' or unit == '厘米':
+    print('%f厘米 = %f英寸' % (value, value / 2.54))
+else:
+    print('请输入有效的单位')
+
+"""
+掷骰子决定做什么事情
+
+"""
+
+from random import randint
+
+face = randint(1, 6)
+if face == 1:
+    result = '唱首歌'
+elif face == 2:
+    result = '跳个舞'
+elif face == 3:
+    result = '学狗叫'
+elif face == 4:
+    result = '做俯卧撑'
+elif face == 5:
+    result = '念绕口令'
+else:
+    result = '讲冷笑话'
+print(result)
+
+"""
+输入月收入和五险一金计算个人所得税
+
+"""
+
+salary = float(input('本月收入: '))
+insurance = float(input('五险一金: '))
+diff = salary - insurance - 3500
+if diff <= 0:
+    rate = 0
+    deduction = 0
+elif diff < 1500:
+    rate = 0.03
+    deduction = 0
+elif diff < 4500:
+    rate = 0.13
+    deduction = 105
+elif diff < 9000:
+    rate = 0.2
+    deduction = 555
+elif diff < 35000:
+    rate = 0.25
+    deduction = 1005
+elif diff < 55000:
+    rate = 0.3
+    deduction = 2755
+elif diff < 80000:
+    rate = 0.35
+    deduction = 5505
+else:
+    rate = 0.45
+    deduction = 13505
+tax = abs(diff * rate - deduction)
+print('个人所得税: ￥%.2f元' % tax)
+print('实际到手收入: ￥%.2f元' % (diff + 3500 - tax))
+
+"""
+输入两个正整数计算最大公约数和最小公倍数
+
+"""
+
+x = int(input('x = '))
+y = int(input('y = '))
+if x > y:
+    x, y = y, x
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print('%d和%d的最大公约数是%d' % (x, y, factor))
+        print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
+        break
+
+
+"""
+打印各种三角形图案
+
+*
+**
+***
+****
+*****
+
+    *
+   **
+  ***
+ ****
+*****
+
+    *
+   ***
+  *****
+ *******
+*********
+
+"""
+
+row = int(input('请输入行数: '))
+for i in range(row):
+    for _ in range(i + 1):
+        print('*', end='')
+    print()
+
+
+for i in range(row):
+    for j in range(row):
+        if j < row - i - 1:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print()
+
+for i in range(row):
+    for _ in range(row - i - 1):
+        print(' ', end='')
+    for _ in range(2 * i + 1):
+        print('*', end='')
+    print()
