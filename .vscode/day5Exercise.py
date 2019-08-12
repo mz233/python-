3c887b236a1a6476cce941941d10aa662d36fdac
mazhuang <mazhuang@xylink.com>
2019-08-12 19:55:36 +0800

    Update day5Exercise.py

---
+++ .vscode/day5Exercise.py
@@ -0,0 +1,48 @@
+"""
+在屏幕上显示跑马灯文字
+"""
+import os
+import time
+
+
+def main():
+    content = '北京欢迎你为你开天辟地…………'
+    while True:
+        # 清理屏幕上的输出
+        os.system('cls')  # os.system('clear')
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] + content[0]
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+"""
+生成指定长度的验证码
+"""
+import random
+
+def generate_code(code_len=4):
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars) - 1
+    code = ''
+    for _ in range(code_len):
+        index = random.randint(0,last_pos)
+        code += all_chars[index]
+    return code 
+"""
+找出列表中第一大和第二大的数字
+"""
+def max2(x):
+    m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+    for index in range(2, len(x)):
+        if x[index] > m1:
+            m2 = m1
+            m1 = x[index]
+        elif x[index] > m2:
+            m2 = x[index]
+    return m1, m2
